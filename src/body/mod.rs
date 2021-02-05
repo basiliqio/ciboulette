8e7599e9c52536e0b53e1f040db6dc10a196e1ba
Francis Le Roy <francis.leroy@protonmail.ch>
2021-02-05 09:33:09 +0100

    File reorganization

---
+++ src/body/mod.rs
@@ -0,0 +1,15 @@
+use super::*;
+
+pub mod bag;
+pub mod check;
+pub mod errors_obj;
+pub mod link;
+pub mod relationship;
+pub mod resource_identifier;
+pub mod resource_obj;
+pub mod resource_obj_selector;
+pub mod resource_type;
+pub mod serde_utils;
+pub mod top_level;
+
+use serde_utils::{handle_ident_in_map_stateful, handle_ident_in_map_stateless};
--- src/lib.rs
+++ src/lib.rs
@@ -1,40 +1,31 @@
 #![warn(clippy::all)]
-mod bag;
-mod check;
+mod body;
 mod config;
 mod errors;
-mod errors_obj;
-mod link;
-mod relationship;
-mod resource_identifier;
-mod resource_obj;
-mod resource_obj_selector;
-mod resource_type;
-mod serde_utils;
 #[cfg(test)]
 mod tests;
-mod top_level;
 
+use body::resource_obj::CibouletteResourceVisitor;
+use body::resource_obj_selector::CibouletteResourceSelectorVisitor;
 use either::Either;
 use getset::{Getters, MutGetters};
 use messy_json::*;
-use resource_obj::CibouletteResourceVisitor;
-use resource_obj_selector::CibouletteResourceSelectorVisitor;
 use serde::{Deserialize, Serialize};
 use serde_json::Value;
-use serde_utils::{handle_ident_in_map_stateful, handle_ident_in_map_stateless};
 use std::borrow::Cow;
 use std::collections::HashMap;
 use std::sync::Arc;
 
-pub use bag::CibouletteBag;
+pub use body::bag::CibouletteBag;
+pub use body::errors_obj::{CibouletteErrorLink, CibouletteErrorObj, CibouletteErrorSource};
+pub use body::link::{CibouletteLink, CibouletteLinkObj};
+pub use body::relationship::CibouletteRelationship;
+pub use body::resource_identifier::{
+    CibouletteResourceIdentifier, CibouletteResourceIdentifierSelector,
+};
+pub use body::resource_obj::CibouletteResource;
+pub use body::resource_obj_selector::CibouletteResourceSelector;
+pub use body::resource_type::CibouletteResourceType;
+pub use body::top_level::CibouletteTopLevel;
 pub use config::CibouletteConfig;
 pub use errors::CibouletteError;
-pub use errors_obj::{CibouletteErrorLink, CibouletteErrorObj, CibouletteErrorSource};
-pub use link::{CibouletteLink, CibouletteLinkObj};
-pub use relationship::CibouletteRelationship;
-pub use resource_identifier::{CibouletteResourceIdentifier, CibouletteResourceIdentifierSelector};
-pub use resource_obj::CibouletteResource;
-pub use resource_obj_selector::CibouletteResourceSelector;
-pub use resource_type::CibouletteResourceType;
-pub use top_level::CibouletteTopLevel;
