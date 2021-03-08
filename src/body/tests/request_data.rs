ae58241478a541f66c7db189614f7d3480818ff1
Francis Le Roy <francis.leroy@protonmail.ch>
2021-03-08 14:34:52 +0100

    Added tests to demonstrate how to update only by identifers

--- src/body/tests/request_data.rs
+++ src/body/tests/request_data.rs
@@ -26,6 +26,68 @@ fn null() {
 }
 
 #[test]
+fn identifier_only() {
+    let bag = gen_bag();
+    const VAL: &str = r#"
+	{
+		"data": {
+			"type": "articles",
+			"id": "6720877a-e27e-4e9e-9ac0-3fff4deb55f2"
+		},
+		"meta":
+		{
+			"self": "comments/6720877a-e27e-4e9e-9ac0-3fff4deb55f2"
+		}
+	}
+	"#;
+    let mut deserializer = serde_json::Deserializer::from_str(VAL);
+    let doc_builder = CibouletteBodyBuilder::deserialize(&mut deserializer)
+        .expect("to parse the json:api document");
+    let doc = doc_builder
+        .build(&bag, &CibouletteIntention::Read)
+        .expect("to build the document");
+    // match doc.data() {
+    //     CibouletteBodyData::Null(x) => assert_eq!(x, &true),
+    //     _ => panic!("data should've been present"),
+    // };
+    println!("{:#?}", doc);
+}
+
+#[test]
+fn identifiers_only() {
+    let bag = gen_bag();
+    const VAL: &str = r#"
+	{
+		"data": [
+			{
+				"type": "articles",
+				"id": "6720877a-e27e-4e9e-9ac0-3fff4deb55f2"
+			},
+			{
+				"type": "articles",
+				"id": "b6f4b482-c8a1-4ffe-8be3-a9e83af9d734"
+			}
+		],
+		"meta":
+		{
+			"self": "comments/6720877a-e27e-4e9e-9ac0-3fff4deb55f2"
+		}
+	}
+	"#;
+    let mut deserializer = serde_json::Deserializer::from_str(VAL);
+    let doc_builder = CibouletteBodyBuilder::deserialize(&mut deserializer)
+        .expect("to parse the json:api document");
+    let doc = doc_builder
+        .build(&bag, &CibouletteIntention::Read)
+        .expect("to build the document");
+    // match doc.data() {
+    //     CibouletteBodyData::Null(x) => assert_eq!(x, &true),
+    //     _ => panic!("data should've been present"),
+    // };
+    println!("{:#?}", doc);
+}
+
+#[test]
 fn absent() {
     let bag = gen_bag();
     const VAL: &str = r#"
